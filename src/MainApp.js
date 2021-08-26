e8231532d4a6efdcbbd1970e33cea759712dfe3d
xamesLab <x2.0bgj@gmail.com>
2021-08-26 13:58:14 +0300

    hashRouter

--- src/MainApp.js
+++ src/MainApp.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Content from "./main_component/Content";
 import Header from "./main_component/Header";
 import InfoBar from "./main_component/InfoBar";
@@ -9,19 +9,19 @@ import ProjectBar from "./main_component/ProjectBar";
 function MainApp() {
   //routing
   return (
-    <BrowserRouter>
-      <div className='main'>
+    <HashRouter>
+      <div className="main">
         <Header />
-        <div className='main__content'>
+        <div className="main__content">
           <InfoBar />
           <LinkBar />
 
           <Content />
           <ProjectBar />
         </div>
-        <div className='main__footer'>&copy; xamesLab 2021</div>
+        <div className="main__footer">&copy; xamesLab 2021</div>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
